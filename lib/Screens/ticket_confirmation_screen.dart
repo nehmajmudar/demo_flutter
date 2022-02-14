cf08e4c1815bd7f7aecd3548e1efb5fc4f232cd2
neh.majmudar@bacancy.com <neh.majmudar@bacancy.com>
2022-02-14 19:58:43 +0530

    Optimized the code with some modifications

--- lib/Screens/ticket_confirmation_screen.dart
+++ lib/Screens/ticket_confirmation_screen.dart
@@ -15,38 +15,29 @@ class _TicketDisplayState extends State<TicketDisplay> {
       body: Column(
         children: [
           Container(
-            padding: EdgeInsets.only(left: 15.0),
-            margin: EdgeInsets.only(right: 250.0,top: 40.0),
+            alignment: Alignment.centerLeft,
+            margin: EdgeInsets.only(left: 15.0,top: 40.0),
             width: MediaQuery.of(context).size.width,
-            height: 70,
             color: Color(0xffFF2b2c31),
-            child: Text("You're confirmed!", style: TextStyle(color: Colors.white,fontSize: 30,fontWeight: FontWeight.bold),),
-          ),
-          SizedBox(
-            width: MediaQuery.of(context).size.width,
-            height: 25,
+            child: Text("You're \nconfirmed!", style: TextStyle(color: Colors.white,fontSize: 30,fontWeight: FontWeight.bold),),
           ),
           Container(
-            margin: EdgeInsets.only(left: 15.0,bottom: 10.0),
+            margin: EdgeInsets.only(left: 15.0,bottom: 10.0,top: 25.0),
             alignment: Alignment.centerLeft,
             child: Text('Return to London',style: TextStyle(color: Colors.white,fontSize: 18.0,fontWeight: FontWeight.bold),),
             color: Color(0xffFF2b2c31),
           ),
           Container(
-            margin: EdgeInsets.only(left: 15.0,bottom: 10.0),
+            margin: EdgeInsets.only(left: 15.0,bottom: 30.0),
             alignment: Alignment.centerLeft,
             child: Text('Itinerary #872389476583',style: TextStyle(color: Colors.grey,fontSize: 10),),
             color: Color(0xffFF2b2c31),
-          ),
-          SizedBox(
-            width: MediaQuery.of(context).size.width,
-            height: 20,
           ),
           Container(
             color: Colors.black,
             margin: EdgeInsets.symmetric(vertical: 20.0,horizontal: 15.0),
             width: MediaQuery.of(context).size.width,
-            height: 2.0,
+            height: 1.0,
           ),
           Container(
             margin: EdgeInsets.only(left: 15.0,bottom: 10.0),
@@ -107,17 +98,13 @@ class _TicketDisplayState extends State<TicketDisplay> {
             ],
           ),
           Container(
-            margin: EdgeInsets.only(left: 15.0,top: 10.0),
+            margin: EdgeInsets.only(left: 15.0,top: 8.0),
             alignment: Alignment.centerLeft,
             child: Text('Air France . 1 traveller . 1 stop',style: TextStyle(color: Colors.grey,fontSize: 10),),
             color: Color(0xffFF2b2c31),
           ),
-          SizedBox(
-            width: MediaQuery.of(context).size.width,
-            height: 23.0,
-          ),
           Container(
-            margin: EdgeInsets.only(left: 15.0,bottom: 10.0),
+            margin: EdgeInsets.only(left: 15.0,bottom: 10.0,top: 23.0),
             alignment: Alignment.centerLeft,
             child: Text('Flight to Singapore',style: TextStyle(color: Colors.white,fontSize: 13,fontWeight: FontWeight.bold),),
             color: Color(0xffFF2b2c31),
@@ -178,7 +165,7 @@ class _TicketDisplayState extends State<TicketDisplay> {
             color: Colors.black,
             margin: EdgeInsets.symmetric(vertical: 20.0,horizontal: 15.0),
             width: MediaQuery.of(context).size.width,
-            height: 2.0,
+            height: 1.0,
           ),
           Row(
             children: [
@@ -194,12 +181,8 @@ class _TicketDisplayState extends State<TicketDisplay> {
               )
             ],
           ),
-          SizedBox(
-            width: MediaQuery.of(context).size.width,
-            height: 90,
-          ),
           Container(
-            margin: EdgeInsets.only(left: 15.0,right: 15.0),
+            margin: EdgeInsets.only(left: 15.0,right: 15.0,top: 90.0),
             height: 40,
             alignment: Alignment.center,
             child: Text('View itinerary',style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 18),),
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:demo_flutter/ticket_confirmation_screen.dart';
+import 'package:demo_flutter/Screens/ticket_confirmation_screen.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
@@ -35,156 +36,205 @@ class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         backgroundColor: Color(0xffFF2b2c31),
         leading: Icon(Icons.arrow_back),
         elevation: 0,
       ),
-      body: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.only(left: 15.0),
-            width: MediaQuery.of(context).size.width,
-            height: 50,
-            color: Color(0xffFF2b2c31),
-            child: Text('Sign Up', style: TextStyle(color: Colors.white,fontSize: 27,fontWeight: FontWeight.bold),),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              TextButton(
-                  style: TextButton.styleFrom(
-                    alignment: Alignment.center,
-                    backgroundColor: Colors.black,
-                    padding: EdgeInsets.symmetric(vertical: 1.5,horizontal: 47.0),
-                  ),
-                  onPressed: (){},
-                  child: Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black),
-                      borderRadius: BorderRadius.circular(30.0)
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.only(left: 15.0,top: 8,bottom: 25.0),
+              width: MediaQuery.of(context).size.width,
+              color: Color(0xffFF2b2c31),
+              child: Text('Sign Up', style: TextStyle(color: Colors.white,fontSize: 27,fontWeight: FontWeight.bold),),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                TextButton(
+                    style: TextButton.styleFrom(
+                      alignment: Alignment.center,
+                      backgroundColor: Colors.black,
+                      padding: EdgeInsets.symmetric(vertical: 1.5,horizontal: 47.0),
                     ),
-                    alignment: Alignment.center,
-                    margin: EdgeInsets.only(left: 15.0),
-                    child: Text('Email',style: TextStyle(color: Colors.white),))
-              ),
-              TextButton(
-                  style: TextButton.styleFrom(
-                    alignment: Alignment.center,
-                    backgroundColor: Colors.black,
-                    padding: EdgeInsets.symmetric(vertical: 1.5,horizontal: 47.0),
-                  ),
-                  onPressed: (){},
-                  child: Container(
-                    alignment: Alignment.center,
-                    margin: EdgeInsets.only(right: 15.0),
-                    child: Text('Phone Number',style: TextStyle(color: Colors.white),))
-              ),
-            ],
-          ),
-          SizedBox(
-            width: MediaQuery.of(context).size.width,
-            height: 10.0,
-          ),
-          Container(
-            margin: EdgeInsets.only(left: 15.0,right: 15.0),
-            child: TextField(
-              decoration: InputDecoration(
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white24),
-                  borderRadius: BorderRadius.all(Radius.circular(15.0))
+                    onPressed: (){},
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black),
+                        borderRadius: BorderRadius.circular(30.0)
+                      ),
+                      alignment: Alignment.center,
+                      margin: EdgeInsets.only(left: 15.0),
+                      child: Text('Email',style: TextStyle(color: Colors.white),))
                 ),
-                hintText: 'Enter your email',
-                hintStyle: TextStyle(color: Colors.white24),
-                fillColor: Color(0xffFF2b2c31),
+                TextButton(
+                    style: TextButton.styleFrom(
+                      alignment: Alignment.center,
+                      backgroundColor: Colors.black,
+                      padding: EdgeInsets.symmetric(vertical: 1.5,horizontal: 47.0),
+                    ),
+                    onPressed: (){},
+                    child: Container(
+                      alignment: Alignment.center,
+                      margin: EdgeInsets.only(right: 15.0),
+                      child: Text('Phone Number',style: TextStyle(color: Colors.white),))
+                ),
+              ],
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 15.0,right: 15.0,top: 10.0,bottom: 15.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white24),
+                    borderRadius: BorderRadius.all(Radius.circular(15.0))
+                  ),
+                  hintText: 'Enter your email',
+                  hintStyle: TextStyle(color: Colors.white24),
+                  fillColor: Color(0xffFF2b2c31),
+                ),
               ),
             ),
-          ),
-          Container(
-            margin: EdgeInsets.only(left: 15.0,right: 15.0,top: 10.0,bottom: 10.0),
-            child: Text('By continuing you agree to the Appka Term of Service and Privacy Policy.',style: TextStyle(color: Colors.white,fontSize: 13),),
-          ),
-          SizedBox(
-            width: MediaQuery.of(context).size.width,
-            height: 10.0,
-          ),
-          TextButton(
-            onPressed: () {
-              Navigator.pushNamed(context, '/ticket_confirmation_screen');
-            },
-            child: Container(
-              margin: EdgeInsets.only(left: 15.0,right: 15.0),
-              height: 50,
+            Center(
+              child: RichText(
+                text: TextSpan(
+                  children: [
+                    TextSpan(
+                      text: 'By continuing you agree to the ',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 13
+                      ),
+                    ),
+                    TextSpan(
+                      text: 'Term of Service ',
+                      style: TextStyle(
+                        fontSize: 13,
+                        color: Color(0xffFFfeb648)
+                      ),
+                      recognizer: TapGestureRecognizer()
+                    ),
+                    TextSpan(
+                      text: 'and ',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 13
+                      ),
+                    ),
+                    TextSpan(
+                      text: 'Privacy Policy',
+                      style: TextStyle(
+                        fontSize: 13,
+                        color: Color(0xffFFfeb648)
+                      ),
+                      recognizer: TapGestureRecognizer()
+                    ),
+                  ]
+                )
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/ticket_confirmation_screen');
+              },
+              child: Container(
+                margin: EdgeInsets.only(left: 8.0,right: 8.0,bottom: 8.5,top: 20.0),
+                height: 50,
+                alignment: Alignment.center,
+                child: Text('Continue',style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 18),),
+                decoration: BoxDecoration(
+                  color: Color(0xffFFfeb648),
+                  borderRadius: BorderRadius.circular(17),
+                ),
+              ),
+            ),
+            Container(
               alignment: Alignment.center,
-              child: Text('Continue',style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 18),),
-              decoration: BoxDecoration(
-                color: Color(0xffFFfeb648),
-                borderRadius: BorderRadius.circular(17),
+              child: Text('or',style: TextStyle(color: Colors.white,fontSize: 13),),
+              color: Color(0xffFF2b2c31),
+            ),
+            TextButton(
+              onPressed: () {  },
+              child: Container(
+                margin: EdgeInsets.only(left: 8.0,right: 8.0,top: 8.5),
+                height: 50,
+                alignment: Alignment.center,
+                child: Text('Continue with Apple',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 15),),
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(17),
+                ),
               ),
             ),
-          ),
-          Container(
-            margin: EdgeInsets.only(left: 15.0,right: 15.0),
-            height: 30,
-            alignment: Alignment.center,
-            child: Text('or',style: TextStyle(color: Colors.white,fontSize: 13),),
-            color: Color(0xffFF2b2c31),
-          ),
-          TextButton(
-            onPressed: () {  },
-            child: Container(
-              margin: EdgeInsets.only(left: 15.0,right: 15.0),
-              height: 50,
-              alignment: Alignment.center,
-              child: Text('Continue with Apple',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 15),),
-              decoration: BoxDecoration(
-                color: Colors.black,
-                borderRadius: BorderRadius.circular(17),
+            // SizedBox(
+            //   // width: MediaQuery.of(context).size.width,
+            //   // height: 7.0,
+            // ),
+            TextButton(
+              onPressed: () {  },
+              child: Container(
+                margin: EdgeInsets.only(left: 8.0,right: 8.0),
+                height: 50,
+                alignment: Alignment.center,
+                child: Text('Continue with Facebook',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 15),),
+                decoration: BoxDecoration(
+                  color: Color(0xffFF4267B2),
+                  borderRadius: BorderRadius.circular(17),
+                ),
               ),
             ),
-          ),
-          // SizedBox(
-          //   // width: MediaQuery.of(context).size.width,
-          //   // height: 7.0,
-          // ),
-          TextButton(
-            onPressed: () {  },
-            child: Container(
-              margin: EdgeInsets.only(left: 15.0,right: 15.0),
-              height: 50,
-              alignment: Alignment.center,
-              child: Text('Continue with Facebook',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 15),),
-              decoration: BoxDecoration(
-                color: Color(0xffFF4267B2),
-                borderRadius: BorderRadius.circular(17),
+            // SizedBox(
+            //   width: MediaQuery.of(context).size.width,
+            //   height: 7.0,
+            // ),
+            TextButton(
+              onPressed: () {  },
+              child: Container(
+                margin: EdgeInsets.only(left: 8.0,right: 8.0,bottom: 23.5),
+                height: 50,
+                alignment: Alignment.center,
+                child: Text('Continue with Google',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 15),),
+                decoration: BoxDecoration(
+                  color: Color(0xffFF497ce5),
+                  borderRadius: BorderRadius.circular(17),
+                ),
               ),
             ),
-          ),
-          // SizedBox(
-          //   width: MediaQuery.of(context).size.width,
-          //   height: 7.0,
-          // ),
-          TextButton(
-            onPressed: () {  },
-            child: Container(
-              margin: EdgeInsets.only(left: 15.0,right: 15.0),
-              height: 50,
-              alignment: Alignment.center,
-              child: Text('Continue with Google',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 15),),
-              decoration: BoxDecoration(
-                color: Color(0xffFF497ce5),
-                borderRadius: BorderRadius.circular(17),
+            // Container(
+            //   margin: EdgeInsets.only(left: 15.0,right: 15.0),
+            //   // height: 60,
+            //   alignment: Alignment.center,
+            //   child: Text('or Login or continue as guest',style: TextStyle(color: Colors.white,fontSize: 13),),
+            //   color: Color(0xffFF2b2c31),
+            // ),
+            Center(
+              child: RichText(
+                text: TextSpan(
+                  children: [
+                    TextSpan(
+                      text: 'Or ',
+                      style: TextStyle(
+                        color: Colors.white,
+                          fontSize: 13
+                      ),
+                    ),
+                    TextSpan(
+                      text: 'Login or continue as guest',
+                      style: TextStyle(
+                          fontSize: 13,
+                          color: Color(0xffFFfeb648)
+                      ),
+                      recognizer: TapGestureRecognizer()
+                    ),
+                  ]
+                )
               ),
             ),
-          ),
-          Container(
-            margin: EdgeInsets.only(left: 15.0,right: 15.0),
-            height: 60,
-            alignment: Alignment.center,
-            child: Text('or Login or continue as guest',style: TextStyle(color: Colors.white,fontSize: 13),),
-            color: Color(0xffFF2b2c31),
-          ),
-        ],
+          ],
+        ),
       ),
       backgroundColor: Color(0xffFF2b2c31),
     );
